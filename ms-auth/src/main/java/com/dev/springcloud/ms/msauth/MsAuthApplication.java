87e25a9a9c69b5f0386fa2f2165b3871c1c5ef3f
Miguel Cortes <miguel.cortes@ias.com.co>
2022-12-02 10:44:18 -0500

    added EnableDiscoveryClient, plugin jib to push image at docker hub

--- ms-auth/src/main/java/com/dev/springcloud/ms/msauth/MsAuthApplication.java
+++ ms-auth/src/main/java/com/dev/springcloud/ms/msauth/MsAuthApplication.java
@@ -2,7 +2,9 @@ package com.dev.springcloud.ms.msauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsAuthApplication {
 
